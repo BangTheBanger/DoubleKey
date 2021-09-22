8ffde23e110d45693012f9490e7226dbc2b0852c
Filipe A <nullnight123@gmail.com>
2021-09-22 02:38:45 -0400

    finished version? 1.0

--- Triple Key Value Shift Encryption.py
+++ Triple Key Value Shift Encryption.py
@@ -1,7 +1,48 @@
-from Def_Encode import encode
-from Def_Decode import decode
-
-
+def getkey(origin, val):
+    for key, value in origin.items():
+         if val == value:
+             return key
+def encode(origin, message, masterkey, fkey, fakey):
+    new = ""
+    copies = ""
+    t = True
+    total_reps = ""
+    count = 0
+    for x in message:
+        index = getkey(origin, x)
+        index *= masterkey
+        masterkey += fkey
+        fkey += fakey
+        reps = 0
+        count += 1
+        while t:
+            if (index > 95):
+                index -= 95
+                reps += 1
+            else:
+                if (count == len(message)):
+                    total_reps += str(reps)
+                else:
+                    total_reps += str(reps) + ","
+                new += origin[index]
+                copies += str(reps)
+                break
+    return (new + '\n' + str(total_reps))
+def decode(origin, message, masterkey, fkey, fakey):
+    new = ""
+    reps = message.split('\n')[1]
+    reps = reps.split(',')
+    message = message.split('\n')[0]
+    repkey = 0
+    for x in message:
+        index = getkey(origin, x)
+        index += (95*int(reps[repkey]))
+        index //= masterkey
+        masterkey += fkey
+        fkey += fakey
+        new += origin[index]
+        repkey += 1
+    return new
 origin = {1: ' ', 2: '!', 3: '"', 4: '#', 5: '$', 6: '%', 7: '&', 8: "'", 9: '(', 10: ')',
           11: '*', 12: '+', 13: ',', 14: '-', 15: '.', 16: '/', 17: '0', 18: '1', 19: '2', 20: '3',
           21: '4', 22: '5', 23: '6', 24: '7', 25: '8', 26: '9', 27: ':', 28: ';', 29: '<', 30: '=',
@@ -12,30 +53,20 @@ origin = {1: ' ', 2: '!', 3: '"', 4: '#', 5: '$', 6: '%', 7: '&', 8: "'", 9: '('
           71: 'f', 72: 'g', 73: 'h', 74: 'i', 75: 'j', 76: 'k', 77: 'l', 78: 'm', 79: 'n', 80: 'o',
           81: 'p', 82: 'q', 83: 'r', 84: 's', 85: 't', 86: 'u', 87: 'v', 88: 'w', 89: 'x', 90: 'y',
           91: 'z', 92: '{', 93: '|', 94: '}', 95: '~'}
-
-
-
 message = input("What is the message you want to encode: ")
 mkey = int(input("What is master key: "))
-fkey = int(input("What is your secondary key: "))
-fakey = int(input("What is your tertiary key: "))
+fkey = int(input("What is your fkey: "))
+fakey = int(input("What is your fakey: "))
 code = encode(origin, message, mkey,fkey,fakey)
 print(code)
-
 message = input("What is the message you want to decode: ")
 message += "\n"
 message += input("What is the message you want to decode: ")
-
 mkey = int(input("What is master key: "))
-fkey = int(input("What is your secondary key: "))
-fakey = int(input("What is your tertiary key: "))
+fkey = int(input("What is your fkey: "))
+fakey = int(input("What is your fakey: "))
 code = decode(origin, message, mkey,fkey,fakey)
 print(code)
-
-
-
-
-
 """
 code = encode(origin, "This is my message, I hope it's encrypted", 5, 2, 10)
 print("\nFinal ==" + code)
